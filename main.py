42c3b2bf9f0e61949bdd5c1d9e454a65db886b07
Siim Liiser <siim@wisemedia.ee>
2015-04-05 22:41:34 +0300

    lisasin termo mõõtmise ka liikumise juurde

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import sys
 import serial
 import time
+import termo
 
 def setup():
   if(len(sys.argv) != 2):
     print('Please provide 1 argument, the serial port')
     exit()
+  global mootor
+  mootor =  serial.Serial(sys.argv[1])
 
-  return serial.Serial(sys.argv[1])
+def close():
+  mootor.close()
 
 def goto(x,y):
-  s.write("X%dgY%dg" % (x, y))
+  mootor.write("X%dgY%dg" % (x, y))
 
 
-def scanArea(x0, y0, x1, y1, step):
+def scanArea(x0, y0, x1, y1):
   goto(x0, y0);
-  for y in range(y0, y1, step):
-    rang = (range(x0, x1, step) if y % 2*step == 0 else range(x1, x0, -step))
+  for y in range(y0, y1, STEP):
+    rang = (range(x0, x1, STEP) if y % 2*STEP == 0 else range(x1, x0, -STEP))
     for x in rang:
       goto(x,y)
       time.sleep(SLEEP_TIME)
+      print termo.temp()
 
-X = 500
-Y = 500
-X0 = 0
-Y0 = 0
+X, Y = 500, 500
+X0, Y0 = 0,0
+STEP = 100
 SLEEP_TIME = 1
 
-s = setup()
+setup()
+termo.setup()
 
-scanArea(X0-X/2, Y0-Y/2, X0+X/2, Y0+Y/2, 100)
+scanArea(X0-X/2, Y0-Y/2, X0+X/2, Y0+Y/2)
 
-s.close()
+close()
+termo.close()
--- termo.py
+++ termo.py
@@ -1,22 +1,20 @@
 import u12
-import time
 
-d = u12.U12()
+def setup():
+  global termomeeter
+  termomeeter = u12.U12()
 
-def mV(n):
-  return str(v(n)*1000) + " mV"
+def close():
+  termomeeter.close()
 
 def v(n):
-  return d.eAnalogIn(channel=n, gain=7)['voltage']
+  return termomeeter.eAnalogIn(channel=n, gain=7)['voltage']
 
-def avg(n):
+def avg(n, count):
   summa = 0
-  for i in range(10):
+  for i in range(count):
     summa += v(n)
-  return summa/10.0
+  return summa/float(count)
 
-while(1):
-  print avg(8)
-  print
-  time.sleep(1)
-
+def temp(n = 20):
+  return avg(8, n)*1000
